5a7aa8a417b80acf32a016a7b63c2082e2afabd4
Jocaagura <oficinameltablero@gmail.com>
2023-09-11 17:11:07 -0500

    Ending Freezed I

--- lib/src/domain/models/user/user_model.dart
+++ lib/src/domain/models/user/user_model.dart
@@ -1,49 +1,23 @@
-import 'package:flutter/foundation.dart';
-import 'package:json_annotation/json_annotation.dart';
+// ignore_for_file: invalid_annotation_target
 
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'user_model.freezed.dart';
 part 'user_model.g.dart';
 
 @immutable
-@JsonSerializable()
-class UserModel {
-  const UserModel({
-    required this.id,
-    required this.username,
-    this.avatarPath,
-  });
+@freezed
+class UserModel with _$UserModel {
+  const factory UserModel({
+    required int id,
+    required String username,
+
+    ///
+    @JsonKey(name: 'avatar', fromJson: avatarPathFromJson) String? avatarPath,
+  }) = _UserModel;
+
   factory UserModel.fromJson(Map<String, dynamic> json) =>
       _$UserModelFromJson(json);
-
-  final int id;
-  final String username;
-  @JsonKey(name: 'avatar', fromJson: avatarPathFromJson)
-  final String? avatarPath;
-
-  @override
-  int get hashCode => '$id$username'.hashCode;
-
-  @override
-  bool operator ==(Object other) {
-    return identical(other, this) ||
-        other is UserModel &&
-            runtimeType == other.runtimeType &&
-            id == other.id &&
-            username == other.username;
-  }
-
-  UserModel copyWith({
-    String? username,
-    int? id,
-    String? avatarPath,
-  }) {
-    return UserModel(
-      id: id ?? this.id,
-      username: username ?? this.username,
-      avatarPath: avatarPath ?? this.avatarPath,
-    );
-  }
-
-  Map<String, dynamic> toJson() => _$UserModelToJson(this);
 }
 
 String? avatarPathFromJson(Map<String, dynamic> json) {
